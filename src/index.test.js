c1535c2b89b0919e520ce111d28750f53f63cd96
agguzman <a.guillermoguzman@gmail.com>
2017-02-07 01:30:27 -0500

    refactore import and func name

--- src/index.test.js
+++ src/index.test.js
@@ -1,11 +1,11 @@
 import test from 'ava';
-import create_sdk from './';
+import createSdk from './';
 
 test('sdk is a function constructor', t => {
-    t.true(typeof(create_sdk) === 'function')
+    t.true(typeof(createSdk) === 'function')
 });
 
 test('create_sdk returns an instance of the sdk', t => {
-    const sdk = create_sdk();
+    const sdk = createSdk();
     t.true(sdk.hasOwnProperty("create"));
 });
