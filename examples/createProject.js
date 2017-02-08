7444a4bd4c9e26308515a94356309064477b8c1b
agguzman <a.guillermoguzman@gmail.com>
2017-02-07 21:14:25 -0500

    rename files

---
+++ examples/createProject.js
@@ -0,0 +1,80 @@
+import createSdk from './../dist/index';
+
+const sdk = createSdk("localhost", "8080", "http", "58741bdea64571262822d365");
+
+export const createProject = () => {
+    sdk.create('project', {
+        name: 'project molly from sdk'
+    }).then(function(response) {
+        console.log(response.data)
+    }).catch(function(error) {
+        console.log(error);
+    });
+};
+
+export const createCloud = () => {
+    sdk.create('cloud', {
+        name: 'cloud rainy from sdk',
+        provider: 'Amazon AWS',
+        apiUrl: 'localhost',
+        apiProtocol: 'https'
+    }).then(function(response) {
+        console.log(response.data)
+    }).catch(function(error) {
+        console.log(error);
+    });
+};
+
+export const createAsset = () => {
+    sdk.create('project', {
+        name: 'project 1aaa from sdk'
+    }).then(function(response) {
+        console.log(response.data)
+    }).catch(function(error) {
+        console.log(error);
+    });
+};
+
+export const createAccount = () => {
+    sdk.create('project', {
+        name: 'account bank from sdk',
+        login: 'myLogin',
+        password: 'myPassWord',
+        provider: 'Azure',
+        defaultCloud: 'some nonexistant account'
+    }).then(function(response) {
+        console.log(response.data)
+    }).catch(function(error) {
+        console.log(error);
+    });
+};
+
+export const createTag = () => {
+    sdk.create('project', {
+        name: 'project 1aaa from sdk'
+    }).then(function(response) {
+        console.log(response.data)
+    }).catch(function(error) {
+        console.log(error);
+    });
+};
+
+export const createUser = () => {
+    sdk.create('project', {
+        name: 'project 1aaa from sdk'
+    }).then(function(response) {
+        console.log(response.data)
+    }).catch(function(error) {
+        console.log(error);
+    });
+};
+
+export const createCredential = () => {
+    sdk.create('project', {
+        name: 'project 1aaa from sdk'
+    }).then(function(response) {
+        console.log(response.data)
+    }).catch(function(error) {
+        console.log(error);
+    });
+};
