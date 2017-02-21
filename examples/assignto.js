6cdaf88d444356166667d36e09135ca6b194dd58
agguzman <a.guillermoguzman@gmail.com>
2017-02-20 20:39:18 -0500

    rename assignto example

--- examples/assignto.js
+++ examples/assignto.js
@@ -1,4 +1,4 @@
-import sdk, { axiosConnector, testAssignto } from './../src';
+import sdk, { axiosConnector } from './../src';
 import axios from 'axios'
 
 
@@ -6,12 +6,16 @@ const axiosConnectedSdk = axiosConnector(axios)(sdk);
 const ctm = axiosConnectedSdk('localhost', 8080, 'http').withCreds('Administrator', 'Password1');
 
 
+const testAssignTo = (response, assignees) => {
+    const assigneeIncluded = (assignee) => response.data.Response[0].plugin.args.assignto.includes(assignee);
+    return assignees.every(assigneeIncluded);
+};
 
 
+// project name, assignees on a pipeline, and the pipeline
 
 const PROJECT = 'Project-1-for-testing';
 const ASSIGN_TO = ['administrator'];
-
 const PIPELINE = {
     'actions': [],
     'dependencies': [],
@@ -54,40 +58,66 @@ const PIPELINE = {
 };
 
 
-// ctm.create('project', {name: PROJECT})
-//     .then(() => {
-//
-//         ctm.impo('pipeline', PIPELINE)
-//             .then(() => {
-//
-//                 ctm.initiate('pipeline', {
-//                     definition: PIPELINE['name'],
-//                     project: PROJECT,
-//                     group: 'debug'
-//                 }).then(() => {
-//
-//                     ctm.get('worklist',{
-//                         filter: PIPELINE['name']
-//                     }).then((response) => {
-//
-//                             const actual = testAssignto(response, ASSIGN_TO);
-//                             console.log(`Pass: ${actual}`);
-//
-//                         }).catch(console.log)
-//                     }).catch(console.log)
-//                 }).catch(console.log)
-//         }).catch(console.log);
+ctm.create('project', { name: PROJECT })
+    .then(() => {
+
+        ctm.impo('pipeline', { backup: PIPELINE })
+            .then(() => {
+
+                ctm.initiate('pipeline', {
+                    definition: PIPELINE['name'],
+                    project: PROJECT,
+                    group: 'debug'
+                }).then(response => {
+
+                    const name = response.data.Response.name;
+
+                    ctm.get('worklist', {
+                        filter: name
+                    }).then(response => {
+
+                            const actual = testAssignTo(response, ASSIGN_TO);
+                            console.log(`Pass: ${actual}`);
+
+                        }).catch(console.log)
+                }).catch(console.log)
+                }).catch(console.log)
+        }).catch(console.log);
 
 
-ctm.get('worklist', {})
-    .then(response => {
-        console.log(response);
-        const actual = testAssignto(response, ASSIGN_TO);
-        console.log(actual)
-    });
+// ctm.get('worklist', {})
+//     .then(response => {
+//         console.dir(response, {depth: null});
+//         // const actual = testAssignTo(response, ASSIGN_TO);
+//         // console.log(actual)
+//     });
 
-// jasmine.DEFAULT_TIMEOUT_INTERVAL = 50000;
+// ctm.initiate('pipeline', {
+//     definition: PIPELINE['name'],
+//     project: PROJECT,
+//     group: 'debug2'
+// }).then((response) => {
 //
+//     const id = response.data.Response._id;
+//     console.dir(id, {depth: null});
+//
+//     ctm.get('worklist', {})
+//         .then(response => {
+//
+//
+//             const pipelineAssignees = response.data.Response.filter(pipeline_instance => {
+//
+//                 console.log('rc_id', pipeline_instance.rc_id)
+//
+//                 if(pipeline_instance.rc_id === id) {
+//                     console.log('found')
+//                 }
+//             });
+//
+//         });
+//
+// });
+
 // test('Assignees should be present in a pending pipeline', done => {
 //
 //     return ctm.get('worklist', {})
@@ -101,3 +131,4 @@ ctm.get('worklist', {})
 //             done()
 //         })
 // });
+
