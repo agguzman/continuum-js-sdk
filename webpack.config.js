4f52b9cde4928a96e9ab620be923be113d29da0b
Alejandro Guzman <alejandro.guzman@versionone.com>
2017-02-22 10:33:25 -0500

    change name of build directory to dist and root file to index

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ var webpack = require('webpack');
 var path = require('path');
 
 var APP_DIR = path.resolve(__dirname, 'src');
-var BUILD_DIR = path.resolve(__dirname, 'build');
+var BUILD_DIR = path.resolve(__dirname, 'dist');
 
 var config = {
     entry: APP_DIR,
     output: {
         path: BUILD_DIR,
-        filename: 'bundle.js',
+        filename: 'index.js',
         library: ["CTM"],
         libraryTarget: 'umd'
     },
